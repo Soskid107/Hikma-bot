e8a5b9bd5dab3a3cc6e7bb4f86fb6833cb164a17
Alade Sodiq <salade537@gmail.com>
2025-07-14 17:39:49 +0100

    "NeonDBURL"

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -10,12 +10,7 @@ import { HerbalTip } from '../entities/HerbalTip';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_POSTGRES_URL_NO_SSL,
   entities: [
     User,
     DailyChecklist,
